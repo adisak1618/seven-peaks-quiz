aec136cc6b8fec42e51f1a950d59cbf9212b8758
Adisak Chaiyakul <adisakchaiyakul@gmail.com>
2021-03-22 16:57:32 +0700

    css vars

---
+++ components/css/index.ts
@@ -0,0 +1,16 @@
+export enum font {
+  Title = "'Playfair Display', serif",
+  Description = "'Source Sans Pro', sans-serif"
+}
+
+export enum colour {
+  Main = "#09357b",
+  MainLight = "rgba(9, 53, 123, 0.9)",
+  Blue = "#0d47a1",
+  White = "#ffffff",
+  WhiteSuperLight = "rgba(255, 255, 255, 0.15)",
+  LabelGreen = "#388e3c",
+  LabelLightBlue = "#2196f3",
+  LabelYellow = "#ffc107",
+  LabelRed = "#d32f2f"
+}
